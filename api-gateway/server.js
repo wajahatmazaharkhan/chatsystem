e4849b4ac507917351c8f0bb486571af4c9abf1c
Lakshay Sharma <lakshay0328@gmail.com>
2026-05-30 21:25:46 +0530

    Created integration project structure

--- api-gateway/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./src/app");
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Gateway running on port ${PORT}`);
-});
