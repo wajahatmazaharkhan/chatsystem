87f69e8de266019a33a1ce5faa017e8bfd83642a
Lakshay Sharma <lakshay0328@gmail.com>
2026-05-31 18:13:04 +0530

    Added login route for auth module

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,53 +1,65 @@
 import {
-    BrowserRouter,
-    Routes,
-    Route,
+  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
 
-import Login from '../pages/auth/Login'
-
 import DashboardLayout from "../components/layout/DashboardLayout";
+
+import Login from "../pages/auth/Login";
 
 import BatchCreate from "../pages/BatchCreate";
 import BatchList from "../pages/BatchList";
 import BatchDetails from "../pages/BatchDetails";
 
 import GroupList from "../pages/GroupList";
-
 import GroupDetails from "../pages/GroupDetails";
 
 export default function AppRoutes() {
-    return (
-        <BrowserRouter>
-          <Route path="/login" element={<Login />} />
-            <DashboardLayout>
-                <Routes>
-                    <Route
-                        path="/batches/create"
-                        element={<BatchCreate />}
-                    />
+  return (
+    <BrowserRouter>
+      <Routes>
 
-                    <Route
-                        path="/batches"
-                        element={<BatchList />}
-                    />
+        {/* Login Route */}
+        <Route
+          path="/login"
+          element={<Login />}
+        />
 
-                    <Route
-                        path="/batches/:id"
-                        element={<BatchDetails />}
-                    />
+        {/* Dashboard Routes */}
+        <Route
+          path="/"
+          element={
+            <DashboardLayout />
+          }
+        >
+          <Route
+            path="batches/create"
+            element={<BatchCreate />}
+          />
 
-                    <Route
-                        path="/groups"
-                        element={<GroupList />}
-                    />
+          <Route
+            path="batches"
+            element={<BatchList />}
+          />
 
-                    <Route
-                        path="/groups/:id"
-                        element={<GroupDetails />}
-                    />
-                </Routes>
-            </DashboardLayout>
-        </BrowserRouter>
-    );
+          <Route
+            path="batches/:id"
+            element={<BatchDetails />}
+          />
+
+          <Route
+            path="groups"
+            element={<GroupList />}
+          />
+
+          <Route
+            path="groups/:id"
+            element={<GroupDetails />}
+          />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
